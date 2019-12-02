040dbe7aa0fdeb8fae8512a8075cd2bf91315a07
Hamilton Evans <glennhe@slalom.com>
2019-12-02 10:31:16 -0500

    Code cleanup and chaining

--- day1.js
+++ day1.js
@@ -2,26 +2,24 @@ const _ = require('lodash');
 const input = require('./day1input');
 
 function day1_part1(input) {
-    input = _.map(input, mass => _.floor(mass / 3) - 2)
-    return _.sum(input);
+    return _.chain(input)
+            .map(mass => _.floor(mass / 3) - 2)
+            .sum();
 }
 
 function day1_part2(input) {
-    input = _.map(input, mass => {
-        let fuel = _.floor(mass / 3) - 2;
-        let req = fuel;
-        while (req > 0) {
-            req = _.floor(req / 3) - 2;
-            if (req > 0) {
-                fuel += req;
-            } else {
-                break;
-            }
-        }
-        return fuel;
-    });
-    return _.sum(input);
+    return _.chain(input)
+            .map(mass => {
+                let totalFuel = _.floor(mass / 3) - 2;
+                let fuel = totalFuel;
+                while (fuel > 0) {
+                    fuel = _.floor(fuel / 3) - 2;
+                    totalFuel += (fuel > 0) ? fuel : 0;
+                }
+                return totalFuel;
+            })
+            .sum();
 }
 
-// console.log("Part 1 - " + day1_part1(input));
+console.log("Part 1 - " + day1_part1(input));
 console.log("Part 2 - " + day1_part2(input));
