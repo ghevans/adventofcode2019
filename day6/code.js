d795765edd7cc58fb4140245d77ecca011faeb9d
Hamilton Evans <glennhe@slalom.com>
2019-12-06 09:29:28 -0500

    Interesting solutionf or day 2

--- day6/code.js
+++ day6/code.js
@@ -17,9 +17,21 @@ function getOrbits(orbits, current) {
     return 1 + getOrbits(orbits, orbits[current]);
 }
 
-function part2(input) {
-    return "tbd";
+function part2(orbits) {
+    let santaPath = getPath(orbits, 'SAN', []);
+    let myPath = getPath(orbits, 'YOU', []);
+
+    return (_.difference(myPath, _.intersection(santaPath, myPath)).length +
+            _.difference(santaPath, _.intersection(santaPath, myPath)).length - 2);
+}
+
+function getPath(orbits, current, path) {
+    path.push(current);
+    if (orbits[current] === undefined) {
+        return path;
+    }
+    return getPath(orbits, orbits[current], path);
 }
 
 console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(input));
+console.log("Part 2 - " + part2(input));
