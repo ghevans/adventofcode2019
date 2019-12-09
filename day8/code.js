979a1ab5dad7e5e468a121ab469ffa24e2b17223
Hamilton Evans <glennhe@slalom.com>
2019-12-09 09:39:02 -0500

    Day 8 done, i like this one

--- day8/code.js
+++ day8/code.js
@@ -7,12 +7,12 @@ function part1(input, w, t) {
     for (let i = 0; i < numLayers; i++) {
         let l = input.substring(i*w*t, (1+i)*w*t);
         layers.push({
-            layer: l,
+            layer: _.chunk(l, w),
             zeros: findBy(l, '0'),
             mult: findBy(l, '1') * findBy(l, '2')
         });
     }
-    
+
     return _(layers).minBy('zeros').mult;
 }
 
@@ -20,11 +20,41 @@ function findBy(layer, char) {
     return _.filter(layer.split(''), d => d === char).length;
 }
 
-function part2(input) {
-    return "tbd";
+function part2(input, w, t) {
+    let output = [];
+
+    let layers = [];
+    let numLayers = input.length / (w*t);
+    for (let i = 0; i < numLayers; i++) {
+        let l = input.substring(i*w*t, (1+i)*w*t);
+        layers.push({
+            layer: _.chunk(l, w),
+            zeros: findBy(l, '0'),
+            mult: findBy(l, '1') * findBy(l, '2')
+        });
+    }
+
+    output = layers[0].layer;
+    for (let i = 1; i < numLayers; i++) {
+        for (let j = 0; j < t; j++) {
+            for (let k = 0; k < w; k++) {
+                if (output[j][k] === '2') {
+                    output[j][k] = layers[i].layer[j][k];
+                }
+            }
+        }
+    }
+    let out = '';
+    for (let i = 0; i < t; i++) {
+        let row = '';
+        for(let j = 0; j < w; j++) {
+            row += (output[i][j] == '1') ? output[i][j] : ' ';
+        }
+        out += row + '\n';
+    }
+    
+    return out;
 }
 
-let test = "123456782012";
-// console.log("Part 1 - " + part1(test, 3, 2));
 console.log("Part 1 - " + part1(input, 25, 6));
-// console.log("Part 2 - " + part2(input));
+console.log("Part 2 - \n" + part2(input, 25, 6));
