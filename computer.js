bba6e3883ab21043d3c7591738a972a0df2184d3
Hamilton Evans <glenn.h.evans@gmail.com>
2019-12-14 10:01:32 -0500

    More changes

--- computer.js
+++ computer.js
@@ -7,30 +7,83 @@ const JIT = 5;
 const JIF = 6;
 const LT = 7;
 const EQ = 8;
+const RBO = 9;
 
 const POSITION = 0;
 const IMMEDIATE = 1;
-function determineVal(program, mode, loc) {
+const RELATIVE = 2;
+function determineVal(program, mode, loc, offset) {
     switch (mode) { 
         case POSITION:
-            return program[program[loc]];
+            while (program[program[loc]] === undefined) {
+                program.push(0);
+            }
+            return program[program[loc]]
         case IMMEDIATE:
             return program[loc];
+        case RELATIVE:
+            while (program[program[loc]+offset] === undefined) {
+                program.push(0);
+            }
+            return program[program[loc]+offset];
         default:
             console.log("Unknown mode: " + mode);
     } 
 }
 
+// function getInst(program, loc) {
+//     const inst = program[loc].toString().padStart(5, '0');
+//     return {
+//         opCode: Number(inst.slice(3)),
+//         modes: [
+//             Number(inst[2]),
+//             Number(inst[1]),
+//             Number(inst[0])
+//         ]
+//     }
+// }
+
+// function getVal(program, loc, mode, offset) {
+//     switch (mode) { 
+//         case POSITION:
+//             while (program[program[loc]] === undefined) {
+//                 program.push(0);
+//             }
+//             return program[program[loc]]
+//         case IMMEDIATE:
+//             return program[loc];
+//         case RELATIVE:
+//             while (program[program[loc]+offset] === undefined) {
+//                 program.push(0);
+//             }
+//             return program[program[loc]+offset];
+//         default:
+//             console.log("Unknown mode: " + mode);
+//     }
+// }
+
+// function putVal(program, loc, mode, offset, value) {
+
+// }
+
+// console.log(getInst([203],0))
+
 function* runProgram(program, input) {
-    let loc = 0, output = 0;
+    let loc = 0, output = 0, offset = 0;
     while (program[loc] != END) {
+        // let inst = getInst(program, loc);
+
+
         let inst = program[loc].toString().padStart(5, '0');
         let opCode = Number(inst.slice(3));
-        let left = determineVal(program, Number(inst[2]), loc + 1);
-        let right = determineVal(program, Number(inst[1]), loc + 2);
+        let left = determineVal(program, Number(inst[2]), loc + 1, offset);
+        let right = determineVal(program, Number(inst[1]), loc + 2, offset);
         let dest = program[loc+3];
         switch (opCode) {
             case ADD:
+                // let l = getVal(program, loc+1, inst.modes[0], offset);
+                // let r = getVal(program, loc+2, inst.modes[1], offset);
+                // putVal(program, loc+3, inst.modes[2], offset, (l + r));
                 program[dest] = left + right;
                 loc += 4;
                 break;
@@ -39,11 +92,18 @@ function* runProgram(program, input) {
                 loc += 4;
                 break;
             case IN:
-                program[program[loc+1]] = input.next().value;
+                switch(Number(inst[2])) {
+                    case POSITION:
+                        program[program[loc+1]] = input.next().value;
+                        break;
+                    case RELATIVE:
+                        program[program[loc+1] + offset] = input.next().value;
+                        break;
+                }
                 loc += 2;
                 break;
             case OUT:
-                yield determineVal(program, Number(inst[2]), loc+1);
+                yield determineVal(program, Number(inst[2]), loc+1, offset);
                 loc += 2;
                 break;
             case JIT:
@@ -59,6 +119,10 @@ function* runProgram(program, input) {
             case EQ:
                 program[dest] = (left === right) ? 1 : 0;
                 loc += 4;
+                break;
+            case RBO:
+                offset += left;
+                loc += 2;
                 break;
             default:
                 console.log("ERROR: " + opCode);
