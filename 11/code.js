e4732bc2c9b413b6f59e0a6186275a4dd21c7700
Hamilton Evans <glennhe@slalom.com>
2019-12-16 11:14:53 -0500

    something

--- 11/code.js
+++ 11/code.js
@@ -2,24 +2,52 @@ const _ = require('lodash');
 const input = require('./input');
 const computer = require('../computer');
 
-let ship = [];
-let currentX = 0;
-let currentY = 0;
 
 const black = 0;
 const white = 1;
-const up = 'U';
-const down = 'D';
-const left = 'L';
-const right = 'R';
+const UP    = 'U';
+const DOWN  = 'D';
+const LEFT  = 'L';
+const RIGHT = 'R';
 
-let direction = up;
+let ship = {};
+let currentX = 0;
+let currentY = 0;
+let currentDir = UP;
+
 function* inputGen(start) {
-    yield start;
-
     while(true) {
+        yield ship[`${currentX},${currentY}`] || black;
+    }
+}
 
-        yield ship[x][y] || 0;
+function turnRobot(direction) {
+    switch (currentDir) {
+        case UP:
+            return (direction) ? LEFT : RIGHT;
+        case DOWN:
+            return (direction) ? RIGHT : LEFT;
+        case LEFT:
+            return (direction) ? DOWN : UP;
+        case RIGHT:
+            return (direction) ? UP : DOWN;
+    }
+}
+
+function moveRobot() {
+    switch(currentDir) {
+        case UP:
+            currentY += 1;
+            break;
+        case DOWN:
+            currentY -= 1;
+            break;
+        case LEFT:
+            currentX -= 1;
+            break;
+        case RIGHT:
+            currentX += 1;
+            break;
     }
 }
 
@@ -32,13 +60,24 @@ function part1(program) {
     // run 
     // get output (color to paint)
     // get output (direction to turn)
+    let done = false;
+    while(!done) {
+        let color = robot.next();
+        let movement = robot.next();
+        // console.log(`panel is color ${color.value} and moving ${movement.value}`);
+        ship[`${currentX},${currentY}`] = color.value;
 
+        // console.log('robot turned from ' + currentDir);
+        currentDir = turnRobot(movement.value);
+        // console.log('to ' + currentDir);
 
-    let output = robot.next();
-    while(!output.done) {
-        console.log(output.value);
-        output = robot.next();
+        moveRobot();
+        // console.log(`now located at ${currentX},${currentY}`);
+
+        done = movement.done;
     }
+
+    console.log(ship);
 
     return "tbd";
 }
