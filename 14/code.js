9eefe4d5e170bcc1bed5943a50f845b811a25311
Hamilton Evans <glennhe@slalom.com>
2019-12-19 14:21:26 -0500

    day 14 part 1

--- 14/code.js
+++ 14/code.js
@@ -1,13 +1,55 @@
 const _ = require('lodash');
-const input = require('./input');
+const reactions = require('./input');
 
-function part1(input) {
-    return "tbd";
+// let testMap = new Map();
+// let test = `10 ORE => 10 A
+// 1 ORE => 1 B
+// 7 A, 1 B => 1 C
+// 7 A, 1 C => 1 D
+// 7 A, 1 D => 1 E
+// 7 A, 1 E => 1 FUEL`.split('\n')
+// .forEach(reaction => { 
+//     reaction = reaction.split(" => ");
+
+//     testMap.set(reaction[1].split(' ')[1], {
+//         quant: reaction[1].split(' ')[0],
+//         consumes: reaction[0].split(', ').map(e => { let t = e.split(' '); return { quant: t[0], chem: t[1]}; })
+//     });
+// });
+
+function convert(leftovers, chem, quantity) {
+    let ore = 0;
+    let reaction = reactions.get(chem);
+    let scaler = Math.ceil(quantity / reaction.quant);
+
+    for (let req of reaction.consumes) {
+        let chemName = req.chem;
+        let needs = req.quant * scaler;
+
+        if (chemName === 'ORE') {
+            ore +=  needs;
+        } else {
+            let chemLeftovers = leftovers[chemName] || 0;
+            if (chemLeftovers < needs) {
+                ore += convert(leftovers, chemName, needs - chemLeftovers);
+            }
+
+            leftovers[chemName] -= needs;
+        }
+    }
+
+    leftovers[chem] = (reaction.quant * scaler) + (leftovers[chem] || 0)
+
+    return ore;
+}
+
+function part1() {
+    return convert({}, 'FUEL', 1);
 }
 
 function part2(input) {
     return "tbd";
 }
 
-console.log("Part 1 - " + part1(input));
-console.log("Part 2 - " + part2(input));
+console.log("Part 1 - " + part1());
+// console.log("Part 2 - " + part2(input));
