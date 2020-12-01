8d57413eff9cb94d979869645b7c13489a5d3648
Hamilton Evans <glenn.h.evans@gmail.com>
2020-12-01 09:34:00 -0500

    Day 1 complete, ugly code

--- 2020/1/code.js
+++ 2020/1/code.js
@@ -1,13 +1,27 @@
 const _ = require('lodash');
 const input = require('./input');
 
-function part1(input) {
-    return "tbd";
+function part1(input, match) {
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i+1; j < input.length; j++) {
+            if (parseInt(input[i]) + parseInt(input[j]) == match) {
+                return input[i] * input[j];
+            }
+        }
+    }
 }
 
-function part2(input) {
-    return "tbd";
+function part2(input, match) {
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i+1; j < input.length; j++) {
+            for (let k = j+1; k < input.length; k++) {
+                if (parseInt(input[i]) + parseInt(input[j]) + parseInt(input[k]) == match) {
+                    return input[i] * input[j] * input[k]; 
+                }
+            }
+        }
+    }
 }
 
-console.log("Part 1 - " + part1(input));
-console.log("Part 2 - " + part2(input));
+console.log("Part 1 - " + part1(input, 2020));
+console.log("Part 2 - " + part2(input, 2020));
