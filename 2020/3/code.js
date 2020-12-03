47e518af080523b92eaa0db21f0292d330b11b8c
Hamilton Evans <glenn.h.evans@gmail.com>
2020-12-03 08:19:41 -0500

    Found the bug, fixed, much cleaner

--- 2020/3/code.js
+++ 2020/3/code.js
@@ -10,25 +10,15 @@ function part2(input) {
     let s2 = countTrees(input,1,3);
     let s3 = countTrees(input,1,5);
     let s4 = countTrees(input,1,7);
-    let s5 = countTrees2(input,2,1);
+    let s5 = countTrees(input,2,1);
 
     return s1*s2*s3*s4*s5;
 }
 
 function countTrees(input, down, right) {
-    let trees = 0;
-    for (let i = 0; i < input.length; i+=down) {
-        trees += (input[i].charAt((i*right) % input[i].length) === '#') ? 1 : 0;
-    }
-    return trees;
-}
-
-function countTrees2(input,down,right) {
-    let trees = 0, depth = 0; i = 0, rowLength = input[0].length;
-    while (depth <= input.length) {
-        trees += (input[depth % input.length].charAt((i*right) % rowLength) === '#') ? 1 : 0;
-        depth+=down;
-        i++;
+    let trees = 0, rowLength = input[0].length;
+    for (let y = 0, x = 0; y < input.length; y+=down, x+=right) {
+        trees += (input[y].charAt(x % rowLength) === '#') ? 1 : 0;
     }
     return trees;
 }
