c3695b614f2ecac479ca223eb7f240a5c355064a
Hamilton Evans <glenn.h.evans@gmail.com>
2020-12-04 13:01:12 -0500

    Cleaned up a bit

--- 2020/4/code.js
+++ 2020/4/code.js
@@ -20,13 +20,13 @@ function part2(input) {
         const keys = Object.keys(passport);
 
         if ((keys.length === 8) || (keys.length === 7 && !keys.includes("cid"))) {
-            if( validByr(passport) &&
-                validIyr(passport) &&
-                validEyr(passport) &&
-                validHgt(passport) &&
-                validHcl(passport) &&
-                validEcl(passport) && 
-                validPid(passport)) {
+            if( validByr(passport.byr) &&
+                validIyr(passport.iyr) &&
+                validEyr(passport.eyr) &&
+                validHgt(passport.hgt) &&
+                validHcl(passport.hcl) &&
+                validEcl(passport.ecl) && 
+                validPid(passport.pid)) {
                 valid++;
             }
         }
@@ -35,34 +35,34 @@ function part2(input) {
     return valid;
 }
 
-function validByr(passport) {
-    return checkPart(passport.byr, 4, 1920, 2002);
+function validByr(byr) {
+    return checkPart(byr, 4, 1920, 2002);
 }
 
-function validIyr(passport) {
-    return checkPart(passport.iyr, 4, 2010, 2020);
+function validIyr(iyr) {
+    return checkPart(iyr, 4, 2010, 2020);
 }
 
-function validEyr(passport) {
-    return checkPart(passport.eyr, 4, 2020, 2030)
+function validEyr(eyr) {
+    return checkPart(eyr, 4, 2020, 2030)
 }
 
-function validHgt(passport) {
-    measurement = passport.hgt.slice(0, passport.hgt.length - 2);
-    return (((passport.hgt.indexOf('cm') > 0) && measurement <= 193 && measurement >= 150) ||
-            ((passport.hgt.indexOf('in') > 0) && measurement <= 76 && measurement >= 59));
+function validHgt(hgt) {
+    measurement = hgt.slice(0, hgt.length - 2);
+    return (((hgt.indexOf('cm') > 0) && measurement <= 193 && measurement >= 150) ||
+            ((hgt.indexOf('in') > 0) && measurement <= 76 && measurement >= 59));
 }
 
-function validHcl(passport) {
-    return (passport.hcl.length === 7 && passport.hcl[0] === '#' && (passport.hcl.replace(/[^#a-f0-9]/g,'') === passport.hcl));
+function validHcl(hcl) {
+    return (hcl.length === 7 && hcl[0] === '#' && (hcl.replace(/[^#a-f0-9]/g,'') === hcl));
 }
 
-function validEcl(passport) {
-    return ['amb','blu','brn','gry','grn','hzl','oth'].includes(passport.ecl);
+function validEcl(ecl) {
+    return ['amb','blu','brn','gry','grn','hzl','oth'].includes(ecl);
 }
 
-function validPid(passport) {
-    return ((""+passport.pid).length === 9 && ((""+passport.pid).replace(/[^0-9]/g,'') == passport.pid));
+function validPid(pid) {
+    return ((""+pid).length === 9 && ((""+pid).replace(/[^0-9]/g,'') == pid));
 }
 
 function checkPart(field, length, min, max) {
