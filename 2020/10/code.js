cdb1efd46c90a2b8006d9a6669a45cb5b11a9593
Hamilton Evans <glenn.h.evans@gmail.com>
2020-12-10 12:42:27 -0500

    Ok, that's the last of the refactoring...for now

--- 2020/10/code.js
+++ 2020/10/code.js
@@ -26,15 +26,12 @@ function part1(input) {
 }
 
 function part2(input) {
-    input.push(0);
-    const sorted = input.sort((a,b) => a-b);
-    sorted.push(sorted[sorted.length - 1] + 3);
-
-    let pathsByIndex = Array(sorted.length).fill(0);
-    pathsByIndex[0] = 1; // always 1 path starting from 0
+    const sorted = [...input, 0, _.max(input)+3].sort((a,b) => a-b);
+    let pathsByIndex = [1];
 
     for (let i = 1; i < sorted.length; i++) {
         let start = (i-3 >= 0) ? i-3 : 0;
+        pathsByIndex.push(0);
 
         sorted.slice(start,i)
               .filter(jolt => (jolt + 3 >= sorted[i]))
