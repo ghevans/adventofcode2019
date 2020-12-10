da319c43f8c43998a427e8ab4b28095cb751cc8a
Hamilton Evans <glenn.h.evans@gmail.com>
2020-12-10 08:26:48 -0500

    added a comment bc im dumb

--- 2020/10/code.js
+++ 2020/10/code.js
@@ -34,7 +34,7 @@ function part2(input) {
     pathsByIndex[0] = 1; // always 1 path starting from 0
 
     for (let i = 0; i < sorted.length; i++) {
-        for (let j = 1; j < 4; j++) {
+        for (let j = 1; j <= 3; j++) {
             if (sorted[i-j] + 3 >= sorted[i]) {
                 // console.log(`able to move from ${sorted[i]} => ${sorted[i-j]}`)
                 // console.log(`adding ${pathsByIndex[i-j]} to ${pathsByIndex[i]}`)
@@ -46,5 +46,6 @@ function part2(input) {
     return pathsByIndex[sorted.length - 1];
 }
 
-console.log("Part 1 - " + part1(input));
+// can't run back to back bc the array gets manipulated
+// console.log("Part 1 - " + part1(input));
 console.log("Part 2 - " + part2(input));
