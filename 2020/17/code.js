706410b5947601eae850b0f826f405acaeae85ee
Hamilton Evans <glenn.h.evans@gmail.com>
2020-12-17 13:16:06 -0500

    4d space wasn't too bad afterall

--- 2020/17/code.js
+++ 2020/17/code.js
@@ -89,9 +89,96 @@ function print(active) {
     console.log(out);
 }
 
-function part2(input) {
-    return "tbd";
+function part2(active) {
+    // only keep track of currently active cell locs
+    // each loop:
+    //      build a list of all possible cells that touch ANY of the currently active cells
+    //      eval each option based on the rules
+    // return list.length()
+    let hypercube = new Map();
+    for (a of active) {
+        hypercube.set(`${a.x},${a.y},${a.z},0`, '#')
+    }
+    for (let cycle = 0; cycle < 6; cycle++) {
+        let possible = new Set();
+        for (cell of hypercube.keys()) {
+            buildPossibleCells2(cell, possible);
+        }
+        console.log(`have ${possible.size} to check`);
+
+        // console.log(hypercube);
+        // for (poss of possible) {
+        //     if (hypercube.has(poss)) {
+        //         console.log(`FOUND: ${poss}`)
+        //     }
+        // }
+
+        // for (poss of possible) {
+        //     let test = active.filter(a => a.x === poss.x && a.y === poss.y && a.z === poss.z)[0];
+        //     poss.state = (test != undefined) ? '#' : '.';
+        // }
+        
+        hypercube = evaluatePossibilities2(possible, hypercube);
+        console.log(hypercube.size);
+    }
+    return hypercube.size;
 }
 
-console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(input));
+function buildPossibleCells2(cell, possible) {
+    let parts = cell.split(',').map(Number);
+    for(let w = parts[3]-1; w <= parts[3]+1; w++) {
+        for(let z = parts[2]-1; z <= parts[2]+1; z++) {
+            for (let y = parts[1]-1; y <= parts[1]+1; y++) {
+                for (let x = parts[0]-1; x <= parts[0]+1; x++) {
+                    possible.add(`${x},${y},${z},${w}`)
+                }
+            }
+        }
+    }
+}
+
+function evaluatePossibilities2(possible, hypercube) {
+    let out = new Map();
+
+    for (poss of possible) {
+        let numActive = 0;
+        let parts = poss.split(',').map(Number);
+        for(let w = parts[3]-1; w <= parts[3]+1; w++) {
+            for(let z = parts[2]-1; z <= parts[2]+1; z++) {
+                for (let y = parts[1]-1; y <= parts[1]+1; y++) {
+                    for (let x = parts[0]-1; x <= parts[0]+1; x++) {
+                        numActive += (hypercube.has(`${x},${y},${z},${w}`)) ? 1 : 0;
+                    }
+                }
+            }
+        }
+        // console.log(`${poss} has ${numActive} neighbors`)
+
+        // If a cube is active and exactly 2 or 3 of its neighbors are also active, the cube remains active. Otherwise, the cube becomes inactive.
+        // If a cube is inactive but exactly 3 of its neighbors are active, the cube becomes active. Otherwise, the cube remains inactive.
+        if (hypercube.has(poss)) {
+            if (numActive === 3 || numActive == 4) { // have to increase it to 3/4 bc we're counting ourself if we were previously active
+                out.set(poss, '#');
+            }
+        } else if (numActive === 3) {
+            out.set(poss, '#')
+        }
+
+
+        // switch(poss.state) {
+        //     case '#':
+                
+        //         break;
+        //     case '.':
+        //         if (numActive === 3) {
+        //             poss.state = '#'
+        //             out.push(poss);
+        //         };
+        //         break;
+        // }
+    }
+    return out;
+}
+
+// console.log("Part 1 - " + part1(input));
+console.log("Part 2 - " + part2(input));
