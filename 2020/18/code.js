4b0b2a48f8ebdb04aa42db62418890f40b1fd7ca
Hamilton Evans <glenn.h.evans@gmail.com>
2020-12-18 10:36:37 -0500

    Day 18 in the books, took me a while to understand how to regex into the parens bits

--- 2020/18/code.js
+++ 2020/18/code.js
@@ -1,24 +1,25 @@
 const _ = require('lodash');
 const input = require('./input');
 
-function part1(equations) {
+function start(equations, advanced) {
     let sum = 0;
     for (eq of equations) {
-        let ans = solve(eq);
-        console.log(`${eq} => ${ans}\n`);
+        let ans = solve(eq, advanced);
+        // console.log(`${eq} => ${ans}\n`);
         sum += ans;
     }
     return sum;
 }
 
-
-function solve(eq) {
+function solve(eq, advanced) {
     while(eq.indexOf('(') >= 0) {
         eq = eq.replace(/\(([^()]+)\)/g, match => {
-            return solvePart(match.substring(1, match.length-1));
+            return (advanced) ? solveAdvancedPart(match.substring(1, match.length-1)) 
+                              : solvePart(match.substring(1, match.length-1));
         })
     }
-    return solvePart(eq);
+    return (advanced) ? solveAdvancedPart(eq) 
+                      : solvePart(eq);
 }
 
 function solvePart(eq) {
@@ -31,9 +32,21 @@ function solvePart(eq) {
     return parts[0];
 }
 
-function part2(input) {
-    return "tbd";
+function solveAdvancedPart(eq) {
+    let parts = eq.split(' ');
+    while(parts.indexOf('+') > 0) {
+        let loc = parts.indexOf('+');
+        let ans = eval(parts.slice(loc-1,loc+2).join(''));
+        parts = parts.slice(0,loc-1).concat(ans).concat(parts.slice(loc+2));
+    }
+
+    while(parts.length > 1) {
+        let ans = eval(parts.slice(0, 3).join(''));
+        parts = parts.slice(3);
+        parts.unshift(ans);
+    }
+    return parts[0];
 }
 
-console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(input));
+console.log("Part 1 - " + start(input, false));
+console.log("Part 2 - " + start(input, true));
