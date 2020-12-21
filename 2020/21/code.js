ff9c5876620950edc99860487c486add5406addc
Hamilton Evans <glenn.h.evans@gmail.com>
2020-12-21 12:59:20 -0500

    Day 21 part 2 - got stuck in set iterator hell

--- 2020/21/code.js
+++ 2020/21/code.js
@@ -48,9 +48,59 @@ function part1(foods) {
     return sum;
 }
 
-function part2(input) {
-    return "tbd";
+function part2(foods) {
+    let allIngredients = new Set();
+    let allergenMap = new Map();
+
+    for (food of foods) {
+        for (ingredient of food.ingredients) {
+            allIngredients.add(ingredient);
+        }
+        for (allergen of food.allergens) {
+            allergenMap.set(allergen, _.cloneDeep(allIngredients));
+        }
+    }
+    
+    for (allergen of allergenMap.keys()) {
+        let possibleIngredients = allergenMap.get(allergen);
+        for (let food of foods) {
+            if (food.allergens.has(allergen)) {
+                for (let ingredient of allIngredients) {
+                    if (!food.ingredients.has(ingredient) && possibleIngredients.has(ingredient)) {
+                        possibleIngredients.delete(ingredient);
+                    }
+                }
+            }
+            if(possibleIngredients.size === 1) { 
+                break;
+            }
+        }
+    }
+    
+    let singles = [...allergenMap.values()].filter(s => s.size === 1);
+    let outMap = new Map();
+    while (singles.length > 0) {
+        singles.forEach(single => {
+            let s = [...single][0];
+            for(allergen of allergenMap.keys()) {
+                let ingredients = allergenMap.get(allergen);
+                if (ingredients.size === 1) {
+                    outMap.set(allergen, s);
+                }
+                if (ingredients.has(s)) {
+                    ingredients.delete(s);
+                }
+            }
+        });
+        singles = [...allergenMap.values()].filter(s => s.size === 1);
+    }
+    
+    let list = "";
+    for (ing of new Map([...outMap.entries()].sort()).values()) {
+        list += ing + ','
+    }
+    return list.slice(0,-1);
 }
 
 console.log("Part 1 - " + part1(input));
-// console.log("Part 2 - " + part2(input));
+console.log("Part 2 - " + part2(input));
