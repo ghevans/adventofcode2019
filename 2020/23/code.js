5e295ac2643b5a1eadedd19b608fb601c3d4a1de
Hamilton Evans <glenn.h.evans@gmail.com>
2020-12-24 14:39:10 -0500

    Day 23 part 2, went back and started using the index of the array as it's value and the value as the pointer to the next

--- 2020/23/code.js
+++ 2020/23/code.js
@@ -1,4 +1,3 @@
-const yallist = require('yallist');
 const _ = require('lodash');
 const input = require('./input');
 
@@ -51,32 +50,53 @@ function getDest(cups, curVal) {
     }
 }
 
-function part2(input, moves) {
-    let cups = test;
-    let list = yallist.create();
-    for(let i = 0; i < 1000000; i++) {
-        if (cups[i] !== undefined) {
-            list.push(cups[i])
-        } else {
-            list.push(i);
-        }
+function part2(input, numCups) {
+    let cups = input;
+    let pointers = buildPointers(cups, numCups);
+    let currentCup = pointers[0];
+
+    for(let i = 0; i <= 10000000; i++){
+        let moved1 = pointers[currentCup];
+        let moved2 = pointers[moved1];
+        let moved3 = pointers[moved2];
+
+        let nextIndex = getNextIndex(currentCup, [moved1,moved2,moved3], numCups);
+
+        let temp = pointers[moved3];
+        pointers[moved3] = pointers[nextIndex];
+        pointers[nextIndex] = pointers[currentCup];
+        pointers[currentCup] = temp;
+
+        currentCup = pointers[currentCup];
     }
-    list.get(list.length-1).
-    console.log(`done: ${list.length} | ${list.get(list.length-1)}`)
-    // let curPos = 0;
-    // let cups = test;
-    // for(let i = 0; i < moves; i++) {
-    //     let curVal = cups[curPos];
-    //     let nextVal = cups[(curPos+4)%cups.length];
-    //     let toMove = pickUp(cups,curPos+1);
-    //     _.remove(cups, function(val) { return toMove.includes(val);});
-        
-    //     cups.splice(getDest(cups, curVal)+1, 0, ...toMove);
-    //     curPos = cups.indexOf(nextVal);
-    // }
+    return pointers[1] * pointers[pointers[1]];
+}
+
+function buildPointers(cups, numCups) {
+    let out = [...Array(numCups+1).keys()].map(val => ++val);
+    out[0] = out[out.length-1] = cups[0]; // pointer at 0 and 999999 both go to first cup in our string
+    out[cups[cups.length - 1]] = Math.max(...cups) + 1; // connect last cup to start of the +1 array to million
+
+    for(let i = 0; i < cups.length - 1; i++){
+        out[cups[i]] = cups[i+1];
+    }
     
-    // return getAnswer(cups);
+    return out;
+}
+
+function getNextIndex(curIndex, removedCups, numCups) {
+    if (curIndex !== 1) {
+        let nextIndex = curIndex - 1;
+        while(removedCups.includes(nextIndex)) {
+            nextIndex--;
+        }
+        if(nextIndex === 0) {
+            return numCups;
+        }
+        return nextIndex;
+    } 
+    return numCups;
 }
 
 // console.log("Part 1 - " + part1(input, 100));
-console.log("Part 2 - " + part2(input,10000000));
+console.log("Part 2 - " + part2(input, 1000000));
