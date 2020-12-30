467c73b0a3abcdff56883f1e68387ac3c05f87d5
Hamilton Evans <glenn.h.evans@gmail.com>
2020-12-30 10:10:01 -0500

    day 20, background object built

---
+++ 2020/20/code.js
@@ -0,0 +1,18 @@
+const _ = require('lodash');
+const {input, test} = require('./input');
+
+function part1(grid) {
+    // console.log(grid);
+
+    let tile = grid[0];
+    console.log(tile);
+    tile.rotate();
+    return "tbd";
+}
+
+function part2(input) {
+    return "tbd";
+}
+
+console.log("Part 1 - " + part1(test));
+// console.log("Part 2 - " + part2(input));
