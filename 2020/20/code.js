89d10b960a3c9a393fbefa567628b0d616e99959
Hamilton Evans <glenn.h.evans@gmail.com>
2021-01-02 11:13:52 -0500

    WE'VE GOT A PROPERLY ALIGNED GRID

--- 2020/20/code.js
+++ 2020/20/code.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const Tile = require ('./tile')
 const {input, test} = require('./input');
+const { e } = require('mathjs');
 
 function part1(grid) {
     let edgeMap = findPairs(grid);
@@ -37,17 +38,8 @@ function setNeighbors(grid, tiles) {
     grid.find(i => i.id === tiles[1]).addNeighbor(tiles[0]);
 }
 
-function getMyEdges(edgeMap, tileId) {
-    return Array.from(edgeMap, ([name, tiles]) => ({ name, tiles })).filter(edge => edge.tiles.includes(tileId));
-}
-
-function getTile(grid, tileId) {
-    return grid.find(tile => tile.id === tileId);
-}
-
 function part2(grid) {
     let size = Math.sqrt(grid.length);
-    let edgeMap = findPairs(grid);
     for(tile of grid) {
         tile.updateEdges();
     }
@@ -64,59 +56,94 @@ function part2(grid) {
     }
 
     let topLeft = grid.find(tile => tile.neighborTiles.size === 2 && tile.neighbors.right !== null && tile.neighbors.bottom !== null)
-    // console.log(topLeft)
 
     let orderedGrid = [...Array(size)].map(x=>Array(size))
     orderedGrid[0][0] = topLeft.id;
     let x = y = 0;
     let currentTile = startRowTile = topLeft;
-    Tile.print(currentTile)
-    // while(currentTile.neighbors.down != null) {
-
+    // Tile.print(currentTile)
+    while(currentTile.neighbors.bottom != null || currentTile.neighbors.right !== null) {
         while(currentTile.neighbors.right !== null) {
             let edgeToMatch = currentTile.edges.right;
 
-            console.log(`walking right, trying to match: ${currentTile.edges.right}`)
+            // console.log(`walking right, trying to match: ${edgeToMatch}`)
             let nextTile = grid.find(tile => tile.id === currentTile.neighbors.right);
+            let nextTileIndex = grid.findIndex(tile => tile.id === currentTile.neighbors.right)
 
             if(edgeToMatch !== nextTile.edges.left) {
                 let nextLeftFlipped = nextTile.edges.left.split('').reverse().join('');
                 if(edgeToMatch === nextLeftFlipped) {
-                    console.log(`CORRECTLY ALIGNED AFTER FLIP`)
+                    // console.log(`CORRECTLY ALIGNED AFTER FLIP`)
                     nextTile = nextTile.flipOnX();
                     nextTile.updateEdges();
-                    Tile.print(nextTile);
+                    grid[nextTileIndex] = nextTile;
+                    // Tile.print(nextTile);
                 } else {
                     // need to rotate
                     for(let i = 1; i < 4; i++) {
                         nextTile = nextTile.rotate(1);
                         nextTile.updateEdges();
+                        grid[nextTileIndex] = nextTile;
                         if(edgeToMatch === nextTile.edges.left) {
-                            console.log(`CORRECTLY ALIGNED AFTER ${i} ROTATION(s)`);
-                            Tile.print(nextTile);
+                            // console.log(`CORRECTLY ALIGNED AFTER ${i} ROTATION(s)`);
+                            // Tile.print(nextTile);
                             break;
                         }
                     }
                 }
             } else {
-                console.log(`CORRECTLY ALIGNED WITHOUT UPDATE`)
-                Tile.print(nextTile);
+                // console.log(`CORRECTLY ALIGNED WITHOUT UPDATE`)
+                // Tile.print(nextTile);
             }
 
             x++;
             orderedGrid[y][x] = nextTile.id;
             currentTile = nextTile;
-            console.log(`=====================`)
+            // console.log(`=====================`)
         }       
 
-    //     y++;
-    //     currentTile = 
-    // }
-    // console.log(orderGrid);
-    // console.log(grid.filter(t => orderGrid[0].includes(t.id)))
+        edgeToMatch = startRowTile.edges.bottom;
 
-    // grid.map(tile => console.log(`${(tile.neighborTiles.size ===2) ? 'CORNER': ''} || Tile ${tile.id} || neighbors: ${JSON.stringify(tile.neighbors)}`))
+        let nextTile = grid.find(tile => tile.id === startRowTile.neighbors.bottom);
+        let nextTileIndex = grid.findIndex(tile => tile.id === startRowTile.neighbors.bottom)
 
+        if(nextTile === undefined) {
+            break; // this is when we've hit the bottom and are done
+        }
+
+        if(edgeToMatch !== nextTile.edges.top) {
+            let nextTopFlipped = nextTile.edges.top.split('').reverse().join('');
+            if(edgeToMatch === nextTopFlipped) {
+                // console.log(`CORRECTLY ALIGNED AFTER FLIP`)
+                nextTile = nextTile.flipOnX();
+                nextTile.updateEdges();
+                grid[nextTileIndex] = nextTile;
+                // Tile.print(nextTile);
+            } else {
+                for(let i = 1; i < 4; i++) {
+                    nextTile = nextTile.rotate(1);
+                    nextTile.updateEdges();
+                    grid[nextTileIndex] = nextTile;
+                    if(edgeToMatch === nextTile.edges.top) {
+                        // console.log(`CORRECTLY ALIGNED AFTER ${i} ROTATION(s)`);
+                        // Tile.print(nextTile);
+                        break;
+                    }
+                }
+            }
+        } else {
+            // console.log(`CORRECTLY ALIGNED WITHOUT UPDATE`)
+            // Tile.print(nextTile);
+        }
+        
+        y++; x = 0;
+        orderedGrid[y][x] = nextTile.id;
+        startRowTile = currentTile = nextTile;
+    }
+    // Tile.print(grid.find(t => t.id === 3079))
+    // Tile.print(grid.find(t => t.id === 1171))
+    console.log(orderedGrid);
+    printGrid(grid, orderedGrid);
     // // determine ordering of tiles
     // // remove edges from all tiles
     // // build full grid
@@ -125,15 +152,23 @@ function part2(grid) {
     // // answer will be number of # - numberSeaMonsters*15
 }
 
-// function printGrid(grid, orderedGrid) {
-//     let out = '';
-//     for(let y=0; y < orderedGrid.length;y++) {
-//         for(let x=0; x < orderedGrid[0].length; x++) {
-//             let tile = grid.find(t => t.id === orderedGrid[y][x]);
-//             out[]
-//         }
-//     }
-// }
+function printGrid(grid, orderedGrid) {
+    let out = '';
+    let dim = grid.find(t => t.id === orderedGrid[0][0]).grid.length;
+    for(let y=0; y < orderedGrid.length;y++) {
+        let tileRow = '';        
+        for(let z=0; z < dim; z++ ) {
+            let row = '';
+            for(let x=0; x < orderedGrid[0].length; x++) {
+                let tile = grid.find(t => t.id === orderedGrid[y][x]);
+                row += " | " + tile.grid[z].split('').join(' ');
+            }
+            tileRow += row + '\n';
+        }
+        out += tileRow + '\n'
+    }
+    console.log(out);
+}
 
 // console.log("Part 1 - " + part1(input));
 console.log("Part 2 - " + part2(test));
